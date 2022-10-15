847c2939bb08dd424f9ca3664142bc801c7e02c9
kavishkamk <kavishkamadhushan727@gmail.com>
2022-10-15 19:36:35 +0530

    setup ingress-nginx

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,6 +7,10 @@ const port = 4000;
 
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res, next) => {
+    res.send("hiii");
+})
+
 app.listen(port, () => {
-    console.log("server start on port : " + port);
+    console.log("server listen on port : " + port);
 });
